56f9b150ca30699db3bb3531be9cba0ab57813c0
Magnus Valsgård <32643202+Magnusvals@users.noreply.github.com>
2020-01-05 13:08:59 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Test side</title>
+<style type="text/css">
+body {
+	background-color: blue;
+	
+	}
+	
+	.t1 {
+	font-size: 25px;
+	font-family: "arial";
+	background-color: #0A19A7;
+	padding: 10px; 
+	border-style: solid;
+	border-color: #CA1619;
+	border-radius: 10px;
+
+	
+}
+.t2 {
+	
+}
+</style>
+</head>
+<body>
+	<br> 
+
+	<h class="t1">Header i html</h>
+	<p class="t2">Dette paragraft er laget med HTML 5 i Dreamviewer CC</p>
+	
+</body>
+</html>
